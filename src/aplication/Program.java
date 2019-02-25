3ab06f5a2ed4d0328bb563e95299f9e21b1cb452
Luan Dias Félix <luand_21@hotmail.com>
2019-02-25 16:41:30 -0300

    Finish

---
+++ src/aplication/Program.java
@@ -0,0 +1,61 @@
+package aplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		List<Employee> list = new ArrayList<>();
+
+		System.out.println("How many employes will be registered ?");
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+
+			System.out.println();
+			System.out.println("Employee #:" + i + ":");
+			System.out.println("ID: ");
+			int id = sc.nextInt();
+			System.out.println("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.println("Salary: ");
+			double salary = sc.nextDouble();
+			list.add(new Employee(id, name, salary));
+
+		}
+		
+		System.out.println();
+		System.out.println("Enter the employee id that will have salary increase : ");
+		int id = sc.nextInt();
+		
+		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		
+		if (emp == null) {
+			System.out.println("This id does not exist!");
+		}
+		else {
+			System.out.print("Enter the percentage: ");
+			double percentage = sc.nextDouble();
+			emp.increaseSalary(percentage);
+		}
+		
+		System.out.println();
+		System.out.println("List of employees:");
+		for (Employee obj : list) {
+			System.out.println(obj);
+		}
+		
+		sc.close();
+
+	}
+}
